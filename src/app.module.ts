49e58df0d6cdb8d4045e4c4aaca599874b98b736
eunkangchoi <ekk12mv2@gmail.com>
2022-10-15 02:43:35 +0900

    [INIT] 환경세팅 - 에러 & exception 정의

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
+import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
 import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 
 @Module({
-  imports: [EnvironmentConfigModule, TypeormModule, LoggerModule],
+  imports: [EnvironmentConfigModule, TypeormModule, LoggerModule, ExceptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
