7ee11d8290799891916c0b5a2bba0b4f08ade64c
eunkangchoi <ekk12mv2@gmail.com>
2022-10-15 00:52:51 +0900

    [INIT] 환경세팅작업 - 환경변수 config 관리 코드 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
+import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 
 @Module({
-  imports: [],
+  imports: [EnvironmentConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
