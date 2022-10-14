be6f903e6013b92facb91251508022ac6be74d9d
eunkangchoi <ekk12mv2@gmail.com>
2022-10-15 02:30:14 +0900

    [INIT] 환경세팅 작업 - logger 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
+import { LoggerModule } from './infrastructure/logger/logger.module';
 import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 
 @Module({
-  imports: [EnvironmentConfigModule, TypeormModule],
+  imports: [EnvironmentConfigModule, TypeormModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
